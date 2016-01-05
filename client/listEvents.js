567558c76c871e1a00bd4d05173e72c7a3cccaf1
ymekuria <ymekuria@gmail.com>
2016-01-04 20:11:16 -0800

    updated controllers and view

--- client/listEvents.js
+++ client/listEvents.js
@@ -1,12 +1,14 @@
 angular.module('musicTonight.listEvents', [])
   .controller('listEventsCtrl', function($scope, Concerts){
     $scope.events = {};
+    $scope.eventss =[1,2,4];
+
 
     var init = function () {
     Concerts.getConcerts(94118)
       .then(function (eventsObj) {
-        $scope.events.events = eventsObj;
-        console.log('scope.events:', $scope.events);
+        $scope.events = eventsObj;
+        console.log('scope.events:', $scope.events.events);
       })
       .catch(function (error) {
         console.error(error);
